26232c5b021faf24d04293a49748a5779daf3c5e
bhuezodd <huezo.dd@gmail.com>
2020-07-22 20:44:19 -0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
+import 'platzi_trips_cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        home: PlatziTripsCupertino(),
+      ),
+      bloc: UserBloc(),
+    );
+  }
+}
